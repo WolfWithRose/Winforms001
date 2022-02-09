89537f9e52c5d355bec33d24a49d5a138e2f1b99
WolfWithRose <grimm.online7@gmail.com>
2022-02-09 10:07:05 +0100

    Elso lepesek

--- Simulation/Simulation/Form1.Designer.cs
+++ Simulation/Simulation/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Simulation
+﻿namespace Simulation
 {
     partial class Form1
     {
@@ -29,13 +28,47 @@ namespace Simulation
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Kepernyo = new System.Windows.Forms.PictureBox();
+            this.Imput = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Kepernyo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Kepernyo
+            // 
+            this.Kepernyo.Location = new System.Drawing.Point(12, 12);
+            this.Kepernyo.Name = "Kepernyo";
+            this.Kepernyo.Size = new System.Drawing.Size(1142, 626);
+            this.Kepernyo.TabIndex = 0;
+            this.Kepernyo.TabStop = false;
+            // 
+            // Imput
+            // 
+            this.Imput.Location = new System.Drawing.Point(1394, 12);
+            this.Imput.Name = "Imput";
+            this.Imput.Size = new System.Drawing.Size(75, 23);
+            this.Imput.TabIndex = 1;
+            this.Imput.Text = "Hozzáadás";
+            this.Imput.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1481, 650);
+            this.Controls.Add(this.Imput);
+            this.Controls.Add(this.Kepernyo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Kepernyo)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Kepernyo;
+        private System.Windows.Forms.Button Imput;
     }
 }
 
