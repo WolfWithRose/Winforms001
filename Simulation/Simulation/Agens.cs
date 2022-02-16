c86c74e73e1ef560ad086957a7ce1e98c23fa853
WolfWithRose <grimm.online7@gmail.com>
2022-02-16 10:04:03 +0100

    Agensek fejlődnek..

--- Simulation/Simulation/Agens.cs
+++ Simulation/Simulation/Agens.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +13,15 @@ namespace Simulation
         static List<Agens> agensek = new List<Agens>();
         public string nev;
         public bool nem;
+        public Vektor hely;
+        public Vektor v;
 
+        Label nevcimke;
+        public Brush toll;
+
+        public Agens()
+        {
+
+        }
     }
 }
---
+++ Simulation/Simulation/Vektor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Simulation
+{
+    class Vektor
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vektor(double x, double y) => (X, Y) = (x, y);
+        public Vektor(Point P) => (X, Y) = ((double)P.X, (double)P.Y);
+
+        public static Vektor operator +(Vektor a, Vektor b) => new Vektor(a.X + b.X, a.Y + b.Y);
+        public static Vektor operator -(Vektor a, Vektor b) => new Vektor(a.X - b.X, a.Y - b.Y);
+        public static double operator *(Vektor a, Vektor b) => a.X * b.X + a.Y * b.Y;
+        public static Vektor operator *(double lambda, Vektor b) => new Vektor(lambda * b.X, lambda * b.Y);
+        public static Vektor operator *(Vektor b, double lambda) => lambda * b;
+        public double távolságnégyzete(Vektor b) => (this - b).HosszNégyzet();
+        public static Vektor operator /(Vektor b, double lambda) => (1 / lambda) * b;
+        public double HosszNégyzet() => X * X + Y * Y;
+        public double Hossz() => Math.Sqrt(HosszNégyzet());
+    }
+}
