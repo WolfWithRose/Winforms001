b33ffb74050e003caa23e0aca54edb639501d52e
WolfWithRose <grimm.online7@gmail.com>
2022-03-22 23:19:23 +0100

    Let's get into this

--- Simulation/Simulation/Agens.cs
+++ Simulation/Simulation/Agens.cs
@@ -10,18 +10,24 @@ namespace Simulation
 {
     class Agens
     {
-        static List<Agens> agensek = new List<Agens>();
+        static List<Agens> lista = new List<Agens>();
         public string nev;
         public bool nem;
         public Vektor hely;
-        public Vektor v;
+        public Vektor seb;
+        //public int term;
+        //public int ev;
+        //public int eleje;
+        //public int vege;
 
-        Label nevcimke;
         public Brush toll;
 
-        public Agens()
+        public Agens(string nev, bool nem, Vektor hely, Panel panel)
         {
-
+            this.nev = nev;
+            this.nem = nem;
+            this.hely = hely;
+            this.seb = new Vektor(0, 0);
         }
     }
 }
